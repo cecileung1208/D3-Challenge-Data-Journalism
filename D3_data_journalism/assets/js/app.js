a20d29cccb6c376927adf59f5b65d156dc219650
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-28 14:20:31 -0500

    move app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -37,7 +37,7 @@ d3.csv("assets/data/data.csv").then(function(healthData){
 
   // Create scale functions
   var xLinearScale = d3.scaleLinear()
-  .domain([0, d3.max(healthData, d => d.poverty)])
+  .domain([8, d3.max(healthData, d => d.poverty)])
   .range([0, width]);
 
 
